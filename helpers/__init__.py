09a005caa08ba16f0fb7bdb2cbd2f4b5a10561d5
Amir Ziaee <ziaee.a.a@gmail.com>
2022-02-23 19:23:35 -0800

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -7,15 +7,19 @@ SFS-A68: a dataset for the segmentation of space functions in apartment building
 
 
 
+#### 
+
+
 __author__ = "Amir Ziaee, Georg Suter, Mihael Barada"
 __copyright__ = "Design Computing Group TU Wien, 2022"
 __credits__ = ["Design Computing Group TU Wien"]
-__license__ = "Free For Educational Use" # allgemine
+__license__ = "GNU GENERAL PUBLIC LICENSE Version 3"
 __version__ = "1.0.1"
 __maintainer__ = "Amir Ziaee"
 __email__ = "amir.ziaee@tuwien.ac.at"
 __status__ = "Prototype"
 __description__= "SFS-A68: a dataset for the segmentation of space functions in apartment buildings"
+__Url__= "https://github.com/A2Amir/SFS-A68"
 
 print('# ' + '=' * 78)
 print('Author: ' + __author__)
@@ -27,4 +31,5 @@ print('Maintainer: ' + __maintainer__)
 print('Email: ' + __email__)
 print('Status: ' + __status__)
 print('Description: ' + __description__)
+print('Url: ' + __Url__)
 print('# ' + '=' * 78)
